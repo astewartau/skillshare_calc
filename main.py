5b5eac7adab79e831f182752b38a752538e32cf0
Ashley Stewart <a.stewart.au@gmail.com>
2023-09-27 11:26:46 +1000

    Removed exclamations

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def multiply(a, b):
 
 def divide(a, b):
     if b == 0:
-        return "Division by zero is not allowed!"
+        return "Division by zero is not allowed"
     return a / b
 
 def main():
