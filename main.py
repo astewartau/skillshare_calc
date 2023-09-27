00087e38d136375b980b96bfcd7f363a150fe1e7
Ashley Stewart <a.stewart.au@gmail.com>
2023-09-27 11:25:51 +1000

    Changed to modulus and integrated

--- main.py
+++ main.py
@@ -15,17 +15,12 @@ def divide(a, b):
         return "Division by zero is not allowed!"
     return a / b
 
-def factorial(a):
-    if a < 0:
-        return "Factorial of negative numbers is not allowed!"
-    elif a == 0:
-        return 1
-    else:
-        return a * factorial(a - 1)
+def modulus(a, b):
+    return a % b
 
 def main():
     parser = argparse.ArgumentParser(description="Perform arithmetic operations.")
-    parser.add_argument("operation", type=str, choices=["add", "subtract", "multiply", "divide"],
+    parser.add_argument("operation", type=str, choices=["add", "subtract", "multiply", "divide", "modulus"],
                         help="The arithmetic operation to perform.")
     parser.add_argument("a", type=float, help="First number")
     parser.add_argument("b", type=float, help="Second number")
@@ -40,6 +35,8 @@ def main():
         result = multiply(args.a, args.b)
     elif args.operation == "divide":
         result = divide(args.a, args.b)
+    elif args.operation == "modulus":
+        result = modulus(args.a, args.b)
 
     print(f"Result: {args.a} {args.operation} {args.b} = {result}")
 
